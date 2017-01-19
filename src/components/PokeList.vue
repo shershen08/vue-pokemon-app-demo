d7f49e005f7aca14d81327890bcff7e212f702a5
Mikhail Kuznetcov <michail.kuznetsov@gmail.com>
2017-01-19 10:22:39 +0100

    add empty PokeList component and remove static filling of the Hello

---
+++ src/components/PokeList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="poke-list">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'poke-list',
+  data () {
+    return {
+      pokemons: []
+    }
+  },
+  methods : {
+     
+  },
+  mounted : function(){
+     
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
