51f6d1d4715802eb00c7cb9ead60cdebae77de88
Mikhail Kuznetcov <michail.kuznetsov@gmail.com>
2017-01-19 10:24:42 +0100

    added api data request

--- src/components/PokeList.vue
+++ src/components/PokeList.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="poke-list">
+      {{JSON.stringify(pokemons)}}
   </div>
 </template>
 
@@ -7,18 +8,23 @@
 import Vue from 'vue';
 import axios from 'axios';
 
+
 export default {
   name: 'poke-list',
   data () {
     return {
-      pokemons: []
+      pokemons: [],
+      progressLevel: 0
     }
   },
   methods : {
      
   },
   mounted : function(){
-     
+      axios.get('http://pokeapi.co/api/v2/pokemon/').then((list) => {
+          this.pokemons = list.data.results;
+          this.progressLevel = 80;
+      })
   }
 }
 </script>
